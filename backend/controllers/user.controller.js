bb19112412df8f98b38e6052fc330ec3d95d3747
rahel yabebal <rahelyabebal@gmail.com>
2025-10-06 15:45:30 +0300

    removed unused userRouter import

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,7 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js";
 
 export const test = (req, res) => {
   res.json({
